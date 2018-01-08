d0b1617275a175eca50e589cb89c7b3e49ddc74f
Jordan Kline <jordan.kline@gmail.com>
2018-01-08 20:28:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,17 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+    
   def self.all
     @@all
   end
 
   def save
     self.class.all << self
+  end
+
+  def create
+
   end
 
 end
